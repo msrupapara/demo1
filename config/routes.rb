36ba3664672e3d264309e27c69591df129013a7f
dhavalwooplr <dhaval@wooplr.com>
2017-08-01 12:25:32 +0530

    Omniauth sign in

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks'}
+  #as :user do
+   # get 'sign_in' => 'users/sessions#new', :as => 'sign_in'
+   # get 'sign_up' => 'users/registrations#new', :as => 'sign_up'
+   # get 'sign_out' => 'users/sessions#destroy', :as => 'sign_out'
+  #end
+
+  get 'welcome/index'
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
