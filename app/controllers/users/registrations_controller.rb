6e9af12fd1d8db9a39538b623e7f5cdb1fdc2b4c
dhavalwooplr <dhaval@wooplr.com>
2017-08-18 15:13:29 +0530

    change key

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
   class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_filter :authenticate_user!, :only => :token
+  before_action :authenticate_user!, :only => :token
   # GET /resource/sign_up
   def new
     super
