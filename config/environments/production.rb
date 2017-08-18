a35419092415edb04749e3bde690705222b245c0
dhavalwooplr <dhaval@wooplr.com>
2017-08-18 18:52:40 +0530

    set production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,13 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.default_url_options = {:host => 'demoomniauth.herokuapp.com', :protocol => 'http'}
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = {:host => 'demoomniauth.herokuapp.com', :protocol => 'http'}
+  #config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
+    domain:        'demoomniauth.herokuapp.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
